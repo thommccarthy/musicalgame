746372c9cb3ea92540aa5db5137a5e43e87905d6
Thom McCarthy <68306220+thommccarthy@users.noreply.github.com>
2021-02-06 18:46:31 -0500

    Add files via upload
    Changes some stylings and consolidated the top and bottom time signatures to one box for time signature.

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const modesArray = ["Ionian", "Dorian", "Phrygian",
 					"Aeolian", "Locrian"];
 const partsArray = ["One Part", "Two Parts", "Three Parts"];
 const qualityArray = ["Major", "Minor"];
-const timeSigTop = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",];
-const timeSigBottom = ["2", "4", "8"];
+const timeSigTop = ["2/2", "3/2", "4/2", "5/2", "6/2", "7/2", "8/2", "9/2", "10/2", "11/2", "12/2",
+"2/4", "3/4", "4/4", "5/4", "6/4", "7/4", "8/4", "9/4", "10/4", "11/4", "12/4",
+"2/8", "3/8", "4/8", "5/8", "6/8", "7/8", "8/8", "9/8", "10/8", "11/8", "12/8",];
+// const timeSigBottom = ["2", "4", "8"];
 const tempoArray = [`Grave (25–45 bpm)`,
 `Largo (40–60 bpm)`,
 `Adagio (66–76 bpm)`,
@@ -21,7 +23,7 @@ const tempoArray = [`Grave (25–45 bpm)`,
 `Vivacissimo (172–176 bpm)`,
 `Allegrissimo (172–176 bpm)`,
 `Presto (168–200 bpm)`,
-`Ludicrous speed! (200 bpm +)`,];
+`Ludicrous speed! (200 bpm +)`];
 
 let keysButton = document.getElementById("keysbutton");
 let partsButton = document.getElementById("partsbutton");
@@ -86,5 +88,5 @@ modesButton.addEventListener('click', displayMode, false);
 partsButton.addEventListener('click', displayParts, false);
 qualityButton.addEventListener('click', displayQuality, false);
 timeTopButton.addEventListener('click', displayTimeTop, false);
-timeBottomButton.addEventListener('click', displayTimeBottom, false);
+// timeBottomButton.addEventListener('click', displayTimeBottom, false);
 tempoButton.addEventListener('click', displayTempo, false);
